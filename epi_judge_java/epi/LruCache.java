3e5c96aa776ebd9c8711e5f71157c74d5b618566
Ed Johnson <ed@edwardotis.com>
2019-10-30 14:34:03 -0700

    done

--- epi_judge_java/epi/LruCache.java
+++ epi_judge_java/epi/LruCache.java
@@ -3,21 +3,43 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
-import java.util.List;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ LinkedHashMap for LRU.
+ Or without that.
+ A HashMap whose values are references to Nodes of a LinkedList
+
+ */
 public class LruCache {
-  LruCache(final int capacity) {}
+
+  float defaultLoadFactor = 0.75F;
+//  int capacity = 3;
+  private LinkedHashMap<Integer,Integer> cache;
+  LruCache(final int capacity) {
+    cache = new LinkedHashMap<>(capacity, defaultLoadFactor, true){
+
+      @Override
+      protected boolean removeEldestEntry(Map.Entry eldest) {
+        return this.size() > capacity;
+      }
+    };
+
+  }
   public Integer lookup(Integer key) {
-    // TODO - you fill in here.
-    return 0;
+    return cache.getOrDefault(key, -1);
+
   }
   public void insert(Integer key, Integer value) {
-    // TODO - you fill in here.
-    return;
+    cache.putIfAbsent(key, value);
   }
   public Boolean erase(Object key) {
-    // TODO - you fill in here.
-    return true;
+    //O(n)j b/c it's linkedlist. No, still O(1) b/c we have access to
+    //the Node reference directly. No need to search for it, and it's a doubly ll.
+    return cache.remove(key) != null;
   }
   @EpiUserType(ctorParams = {String.class, int.class, int.class})
   public static class Op {
